32d893f18db7c90b45551c6ed1026c8ca61da3eb
onval <giorgio.valbonesi@studenti.unimi.it>
2018-06-05 21:06:51 +0200

    Load fragment programmatically now

--- app/src/main/java/com/onval/capstone/MainActivity.java
+++ app/src/main/java/com/onval/capstone/MainActivity.java
@@ -3,6 +3,7 @@ package com.onval.capstone;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -21,8 +22,14 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(toolbar);
-
         setCustomTitle(R.layout.actionbar_title);
+
+        //TODO: Cursor = getData()
+        //TODO: if (Cursor is empty) -> open EmptyActivity
+        //TODO: else -> open MainFragment
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.add(R.id.fragment_container, new MainFragment());
+        ft.commit();
     }
 
     @OnClick(R.id.main_fab)
