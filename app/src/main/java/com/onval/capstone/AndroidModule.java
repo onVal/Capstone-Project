1e4e08ed797eb35859af56d68de5854ae0d0274f
onval <giorgio.valbonesi@studenti.unimi.it>
2018-06-09 22:09:18 +0200

    Changed launcher icon to a slicker one

---
+++ app/src/main/java/com/onval/capstone/AndroidModule.java
@@ -0,0 +1,19 @@
+package com.onval.capstone;
+
+import dagger.Module;
+
+@Module
+public class AndroidModule {
+    public AndroidModule() {
+
+    }
+
+//    public AppCompatActivity getActivity() {
+////        return getActivity();
+//    }
+
+//    @Provides
+//    public CategoriesViewModel getCategoriesViewModel() {
+//        return ViewModelProviders.of(getActivity()).get(CategoriesViewModel.class);
+//    }
+}
