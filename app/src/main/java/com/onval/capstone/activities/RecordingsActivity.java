3ba22e55a523f1d7d77a10ec5f52244ec1dc4941
onval <giorgio.valbonesi@gmail.com>
2018-08-23 13:41:20 +0200

    Add simple menu to recordings toolbar

--- app/src/main/java/com/onval/capstone/activities/RecordingsActivity.java
+++ app/src/main/java/com/onval/capstone/activities/RecordingsActivity.java
@@ -1,9 +1,15 @@
 package com.onval.capstone.activities;
 
 import android.content.Intent;
+import android.support.annotation.LayoutRes;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
+import android.widget.TextView;
 
 import com.onval.capstone.R;
 import com.onval.capstone.fragment.RecordingsFragment;
@@ -17,8 +23,17 @@ public class RecordingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recordings);
 
+        Toolbar toolbar = findViewById(R.id.my_rec_toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.showOverflowMenu();
+
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setTitle("Recordings");
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setDisplayShowHomeEnabled(true);
+
         getSupportFragmentManager().beginTransaction()
-                .add(R.id.category_container, new RecordingsFragment())
+                .replace(R.id.category_container, new RecordingsFragment())
                 .commit();
     }
 
@@ -26,5 +41,11 @@ public class RecordingsActivity extends AppCompatActivity {
     public void record(View view) {
         Intent intent = new Intent(this, RecordActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_recs, menu);
+        return true;
     }
 }
