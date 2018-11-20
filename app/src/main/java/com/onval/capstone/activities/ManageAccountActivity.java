7461a4bef9246d63a88eef2f5ebfdec7f97fa582
onval <giorgio.valbonesi@gmail.com>
2018-11-20 23:41:11 +0100

    Can finally upload audio file

--- app/src/main/java/com/onval/capstone/activities/ManageAccountActivity.java
+++ app/src/main/java/com/onval/capstone/activities/ManageAccountActivity.java
@@ -2,9 +2,11 @@ package com.onval.capstone.activities;
 
 import android.content.Intent;
 
+import android.os.ParcelFileDescriptor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -17,8 +19,28 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 
 import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveClient;
+import com.google.android.gms.drive.DriveContents;
+import com.google.android.gms.drive.DriveFile;
+import com.google.android.gms.drive.DriveFolder;
+import com.google.android.gms.drive.DriveResourceClient;
+import com.google.android.gms.drive.MetadataChangeSet;
+import com.google.android.gms.drive.metadata.CustomPropertyKey;
 import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
 import com.onval.capstone.R;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.Date;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -28,6 +50,7 @@ public class ManageAccountActivity extends AppCompatActivity {
     public static final int SIGNIN_REQUEST_CODE = 1;
 
     private GoogleSignInClient googleSignInClient;
+    private GoogleSignInAccount googleSignInAccount;
 
     @BindView(R.id.account_name)
     public TextView accountName;
@@ -93,6 +116,7 @@ public class ManageAccountActivity extends AppCompatActivity {
     private void updateViewWithGoogleSignInAccountTask(Task<GoogleSignInAccount> task) {
         task.addOnSuccessListener(
                 googleSignInAccount -> {
+                    this.googleSignInAccount = googleSignInAccount;
                     String accountName = googleSignInAccount.getGivenName();
                     String accountLastName= googleSignInAccount.getFamilyName();
                     signedInName = accountName + " " + accountLastName;
@@ -139,5 +163,51 @@ public class ManageAccountActivity extends AppCompatActivity {
     public void clickLogout() {
         Task<Void> task = googleSignInClient.signOut();
         task.addOnSuccessListener(o -> displayNoUser());
+    }
+
+    @OnClick(R.id.hellobtn)
+    public void clickHello() throws IOException {
+        //upload 1_hhh.mp4 to google drive
+        String path = "/storage/emulated/0/Android/data/com.onval.capstone/cache/";
+        File hello = new File(path + "1_hhh.mp4");
+
+        DriveResourceClient resourceClient =  Drive.getDriveResourceClient(this, googleSignInAccount);
+
+        final Task<DriveFolder> rootFolderTask = resourceClient.getRootFolder();
+        final Task<DriveContents> createContentsTask = resourceClient.createContents();
+        Tasks.whenAll(rootFolderTask, createContentsTask)
+                .continueWithTask(task -> {
+                    DriveFolder parent = rootFolderTask.getResult();
+                    DriveContents contents = createContentsTask.getResult();
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+                    FileInputStream inputStream = new FileInputStream(hello);
+                    int readByte;
+                    while ((readByte = inputStream.read()) != -1) {
+                        baos.write(readByte);
+                    }
+
+                    OutputStream outputStream = contents.getOutputStream();
+                    outputStream.write(baos.toByteArray());
+
+
+                    MetadataChangeSet changeSet = new MetadataChangeSet.Builder()
+                            .setTitle("Hello.mp4")
+                            .setMimeType("audio/mp4")
+                            .setStarred(false)
+                            .build();
+
+                    return resourceClient.createFile(parent, changeSet, contents);
+                })
+                .addOnSuccessListener(this,
+                        driveFile -> {
+                            Log.e("derp", "File created successfully");
+
+                            finish();
+                        })
+                .addOnFailureListener(this, e -> {
+                    Log.e("derp", "Unable to create file", e);
+                    finish();
+                });
     }
 }
