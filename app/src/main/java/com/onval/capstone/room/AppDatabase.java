d2633daa59dad50b8e344f06d5b48a80f31ce2c7
onval <giorgio.valbonesi@gmail.com>
2018-12-22 20:45:38 +0100

    Fixed database mess up

--- app/src/main/java/com/onval/capstone/room/AppDatabase.java
+++ app/src/main/java/com/onval/capstone/room/AppDatabase.java
@@ -5,30 +5,19 @@ import android.content.Context;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
-import androidx.room.migration.Migration;
-import androidx.sqlite.db.SupportSQLiteDatabase;
 
-@Database(entities = {Category.class, Record.class}, version = 3, exportSchema = false)
+@Database(entities = {Category.class, Record.class}, version = 1, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract MyDao getDao();
 
     private static volatile AppDatabase INSTANCE;
-
-    private static final Migration MIGRATION_1_2 = new Migration(1, 2) {
-        @Override
-        public void migrate(SupportSQLiteDatabase database) {
-            database.execSQL("CREATE UNIQUE INDEX index_Record_name ON Record(name)");
-        }
-    };
 
     public static AppDatabase getDatabase(final Context context) {
         if (INSTANCE == null) {
             synchronized (AppDatabase.class) {
                 if (INSTANCE == null) {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
-                            AppDatabase.class, "app_database")
-                            .addMigrations(MIGRATION_1_2)
-                            .fallbackToDestructiveMigration()
+                            AppDatabase.class, "onrec_database")
                             .build();
                 }
             }
